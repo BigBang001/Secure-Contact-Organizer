6c85ab51cd5e8f4345c385ff16b566e822227e6d
Ekamjot Kaur <119509166+BigBang001@users.noreply.github.com>
2024-07-13 19:00:28 +0530

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,28 +1,48 @@
-#include "user.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
-// Security questions list (static)
+class SecurityQuestion {
+public:
+    std::string question;
+    std::string answer;
+
+    SecurityQuestion(const std::string& q, const std::string& a)
+        : question(q), answer(a) {}
+};
+
+class User {
+private:
+    std::string username;
+    std::string password;
+    SecurityQuestion securityQuestion;
+
+public:
+    static const std::vector<SecurityQuestion> securityQuestions;
+
+    User(const std::string& uname, const std::string& pword, const SecurityQuestion& secQ)
+        : username(uname), password(pword), securityQuestion(secQ) {}
+
+    bool authenticate(const std::string& uname, const std::string& pword) const {
+        return (uname == username && pword == password);
+    }
+
+    bool resetPassword(const std::string& answer, const std::string& newPass) {
+        if (answer == securityQuestion.answer) {
+            password = newPass;
+            return true;
+        }
+        return false;
+    }
+
+    const std::string& getSecurityQuestion() const {
+        return securityQuestion.question;
+    }
+};
+
 const std::vector<SecurityQuestion> User::securityQuestions = {
     {"What is your favorite book?", "book"},
     {"What is your mother's maiden name?", "mother"},
     {"Where did you go for your studies?", "studies"},
     {"In which city were you born?", "city"}
 };
-
-User::User(const std::string& uname, const std::string& pword, const SecurityQuestion& secQ)
-    : username(uname), password(pword), securityQuestion(secQ) {}
-
-bool User::authenticate(const std::string& uname, const std::string& pword) const {
-    return (uname == username && pword == password);
-}
-
-bool User::resetPassword(const std::string& answer, const std::string& newPass) {
-    if (answer == securityQuestion.answer) {
-        password = newPass;
-        return true;
-    }
-    return false;
-}
-
-const std::string& User::getSecurityQuestion() const {
-    return securityQuestion.question;
-}
